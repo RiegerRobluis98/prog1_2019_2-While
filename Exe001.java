49a6fb76005b64e01fef4874759f5e19b80466ac
Luiz Roberto C. Rieger <45156462+RiegerRobluis98@users.noreply.github.com>
2019-10-15 09:25:46 -0300

    Create Exe001.java

---
+++ Exe001.java
@@ -0,0 +1,13 @@
+package cont0a100;
+
+public class Cont0a100 {
+
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador <= 100) {
+            System.out.println(contador);
+            contador = contador + 1;
+        }
+    }
+
+}
