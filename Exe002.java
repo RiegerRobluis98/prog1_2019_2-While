a86ee66d3cd743c4fd7a898359ea91d623c0dc83
Luiz Roberto C. Rieger <45156462+RiegerRobluis98@users.noreply.github.com>
2019-10-15 11:41:26 -0300

    Add files via upload

---
+++ Exe002.java
@@ -0,0 +1,14 @@
+package Lista03;
+
+public class Exe002 {
+
+    public static void main(String[] args) {
+        int multi = 0;
+        while (multi < 100) {
+            multi++;
+            if (multi % 3 == 0) {
+                System.out.println(multi);
+            }
+        }
+    }
+}
---
+++ cont0a1000de5.java
@@ -0,0 +1,12 @@
+package Lista03;
+
+public class cont0a1000de5 {
+
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador <= 1000) {
+            System.out.println(contador);
+           contador = contador + 5;
+        }
+    }
+}
